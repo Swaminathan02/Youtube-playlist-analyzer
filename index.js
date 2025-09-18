3b3f027b3897a4d2a3b94f24f02a7e9357a5f8f6
Swaminathan PL <srinath0209@gmail.com>
2025-09-18 12:10:49 +0530

    port issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ytpl = require("ytpl");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 app.use(express.static(path.join(__dirname, "public")));
 
 function durationToSeconds(duration) {
